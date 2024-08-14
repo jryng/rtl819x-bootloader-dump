4a3230d246afdb0ffc764b6ac381068211053430
jryng <jeffryan9315@gmail.com>
2024-08-14 06:04:40 -0300

    Update and rename rtl8186tool.py to rtl819xtool.py

--- rtl819xtool.py
+++ rtl819xtool.py
@@ -45,7 +45,7 @@ def wait_prompt(ser):
 
 def memreadblock(ser, addr, size):
 	skip_prompt(ser)
-	ser.write("D %x %d\r" %(addr, size/4))
+	ser.write("DW %x %d\r" %(addr, size/4))
 	buf=''
 	m = False
 	while not m:
